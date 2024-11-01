ce21ffb89ddf9f563c15c8020653abc8f4717d1a
Adrian Infante Gonzalez <40429138+AdrianInfante@users.noreply.github.com>
2024-11-01 11:33:28 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+import winsound
+import os
+import sys
+
+
+target_image_path = "./event.png"
+alert_sound_path = "./alert.wav"  
+
+def find_image_on_screen(image_path):
+    try:
+        # Attempt to locate the image on the screen with a lower confidence level
+        location = pyautogui.locateOnScreen(image_path, confidence=0.6)
+        if location:
+            print("Image found on screen!")
+            winsound.PlaySound(alert_sound_path, winsound.SND_FILENAME) 
+            return True
+        else:
+            return False
+    except pyautogui.ImageNotFoundException:
+        print("Image could not be processed or found.")
+        return False
+
+
+def main():
+    while True:
+        found = find_image_on_screen(target_image_path)
+        
+        if found:
+            print("Restarting script...")
+            time.sleep(2)  
+            # Restart the script
+            os.execv(sys.executable, ['python'] + sys.argv)
+
+        time.sleep(2)  
+    
+
+
+if __name__ == "__main__":
+    main()
